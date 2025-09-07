ec18e9758d4673a886815c3f9587125b70f21c58
aNN-algorithm <kyukyu0314@naver.com>
2025-09-07 23:47:57 +0900

    Feat: 회원가입 및 로그인 폼 제출 기능 구현

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,8 +1,9 @@
-import React, { useState } from "react";
+import React, { useState, FormEvent } from "react";
 import LoginForm from "@/components/login/LoginForm";
 import SubmitButton from "@/components/shared/SubmitButton";
 import ToSignupPromt from "@/components/login/ToSignupPromt";
 import AuthLogoLink from "@/components/shared/AuthLogoLink";
+import axios from "../lib/axios";
 
 const SUBMITBUTTON_WIDTH = "w-[520px]";
 const SUBMITBUTTON_HEIGHT = "h-[50px]";
@@ -13,27 +14,41 @@ export default function login() {
 
   const isValid = email !== "" && password !== "";
 
+  async function handleSubmit(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+
+    try {
+      await axios.post("/auth/login", {
+        email,
+        password,
+      });
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
   return (
     <div className="h-screen flex flex-col justify-center items-center">
       <AuthLogoLink width={200} height={280} />
       <div className="font-pretendard text-[20px] font-medium leading-[32px] text-[#333236] w-full text-center mt-[10px]">
         오늘도 만나서 반가워요!
       </div>
-      <LoginForm
-        email={email}
-        password={password}
-        onEmailChange={setEmail}
-        onPasswordChange={setPassword}
-      />
-      <div className="mt-[24px]">
-        <SubmitButton
-          width={SUBMITBUTTON_WIDTH}
-          height={SUBMITBUTTON_HEIGHT}
-          disabled={!isValid}
-          onClick={() => console.log(email, password)}
-          label="로그인"
+      <form onSubmit={handleSubmit}>
+        <LoginForm
+          email={email}
+          password={password}
+          onEmailChange={setEmail}
+          onPasswordChange={setPassword}
         />
-      </div>
+        <div className="mt-[24px]">
+          <SubmitButton
+            width={SUBMITBUTTON_WIDTH}
+            height={SUBMITBUTTON_HEIGHT}
+            disabled={!isValid}
+            label="로그인"
+          />
+        </div>
+      </form>
       <ToSignupPromt />
     </div>
   );
--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -1,8 +1,9 @@
-import React, { useState } from "react";
+import React, { useState, FormEvent } from "react";
 import AuthLogoLink from "@/components/shared/AuthLogoLink";
 import SignupForm from "@/components/signup/SignupForm";
 import SubmitButton from "@/components/shared/SubmitButton";
 import ToLoginPrompt from "@/components/signup/ToLoginPrompt";
+import axios from "../lib/axios";
 
 const SUBMITBUTTON_WIDTH = "w-[520px]";
 const SUBMITBUTTON_HEIGHT = "h-[50px]";
@@ -20,31 +21,46 @@ export default function signup() {
     confirmPassword !== "" &&
     password === confirmPassword;
 
+  async function handleSubmit(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+
+    try {
+      await axios.post("/users", {
+        nickname,
+        email,
+        password,
+      });
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
   return (
     <div className="flex flex-col justify-center items-center h-screen">
       <AuthLogoLink width={200} height={280} />
       <div className="font-pretendard text-[20px] font-medium leading-[32px] text-[#333236] w-full text-center mt-[10px]">
         첫 방문을 환영합니다!
       </div>
-      <SignupForm
-        email={email}
-        nickname={nickname}
-        password={password}
-        confirmPassword={confirmPassword}
-        onEmailChange={setEmail}
-        onNicknameChange={setNickname}
-        onPasswordChange={setPassword}
-        onConfirmPasswordChange={setConfirmPassword}
-      />
-      <div className="mt-[24px]">
-        <SubmitButton
-          width={SUBMITBUTTON_WIDTH}
-          height={SUBMITBUTTON_HEIGHT}
-          disabled={!isValid}
-          label="가입하기"
-          onClick={() => console.log(email, password)}
+      <form onSubmit={handleSubmit}>
+        <SignupForm
+          email={email}
+          nickname={nickname}
+          password={password}
+          confirmPassword={confirmPassword}
+          onEmailChange={setEmail}
+          onNicknameChange={setNickname}
+          onPasswordChange={setPassword}
+          onConfirmPasswordChange={setConfirmPassword}
         />
-      </div>
+        <div className="mt-[24px]">
+          <SubmitButton
+            width={SUBMITBUTTON_WIDTH}
+            height={SUBMITBUTTON_HEIGHT}
+            disabled={!isValid}
+            label="가입하기"
+          />
+        </div>
+      </form>
       <ToLoginPrompt />
     </div>
   );
