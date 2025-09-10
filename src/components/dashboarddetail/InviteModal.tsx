9f8720d0cd58267fce79f18cfa5c906dff6fbdbc
Ucastle <skaska007@naver.com>
2025-09-10 19:22:10 +0900

    Refactro: 초대 모달 컴포넌트 리팩토링

--- src/components/dashboarddetail/InviteModal.tsx
+++ src/components/dashboarddetail/InviteModal.tsx
@@ -1,12 +1,13 @@
 import React, { useState } from "react";
 import instance from "@/lib/axios";
+import Modal from "@/components/Modal";
 
-const modalbg = "bg-opacity-50 flex justify-center items-center";
-const modalbox = "bg-[#FFFFFF] p-[24px] rounded-[8px] w-[480px] shadow-lg";
-const modalheder = "flex justify-between items-center mb-4";
-const deletebtn =
+// const modalbg = "bg-opacity-50 flex justify-center items-center";
+// const modalbox = "bg-[#FFFFFF] p-[24px] rounded-[8px] w-[480px] shadow-lg";
+// const modalheder = "flex justify-between items-center mb-4";
+const cancelbtn =
   "bg-[#FFFFFF] text-[#787486] hover:bg-gray-100 rounded-[8px] px-[80px] py-[14px] border-[1px] border-[#d9d9d9]";
-const changebtn = (isDisabled: boolean) =>
+const invitebtn = (isDisabled: boolean) =>
   `${
     isDisabled
       ? "bg-gray-300 cursor-not-allowed"
@@ -15,7 +16,7 @@ const changebtn = (isDisabled: boolean) =>
 
 interface InviteModalProps {
   onClose: () => void;
-  children: React.ReactNode;
+  children?: React.ReactNode;
   dashboardId: number;
 }
 
@@ -46,41 +47,44 @@ export default function InviteModal({
   const isDisabled = !isVaildEmail(email);
 
   return (
-    <div className={modalbg}>
-      <div className={modalbox}>
-        <div className={modalheder}>
-          <h1 className="text-xl font-bold">초대 하기</h1>
-          <button
-            onClick={onClose}
-            className="text-gray-500 hover:text-black font-bold t"
-          >
-            X
-          </button>
-        </div>
-
-        <div className="flex flex-col mb-4">
-          <label className="mb-1 ">이메일</label>
-          <input
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            type="text"
-            className="border rounded px-3 py-2"
-          />
-        </div>
-
-        <div className="flex justify-between">
-          <button onClick={onClose} className={deletebtn}>
-            취소
-          </button>
-          <button
-            onClick={handleInvite}
-            disabled={isDisabled}
-            className={changebtn(isDisabled)}
-          >
-            초대
-          </button>
-        </div>
+    <Modal
+      open={true}
+      onClose={onClose}
+      contentClassName="bg-[#FFFFFF] p-6 rounded-[8px] min-w-[480px]"
+    >
+      <div className="flex justify-between items-center mb-4">
+        <h1 className="text-xl font-bold">초대 하기</h1>
+        <button
+          onClick={onClose}
+          className="text-gray-500 hover:text-black font-bold t"
+        >
+          X
+        </button>
       </div>
-    </div>
+
+      <div className="flex flex-col mb-4">
+        <label className="mb-1 ">이메일</label>
+        <input
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          type="text"
+          className="border rounded px-3 py-2"
+          placeholder="이메일을 입력해 주세요."
+        />
+      </div>
+
+      <div className="flex justify-between">
+        <button onClick={onClose} className={cancelbtn}>
+          취소
+        </button>
+        <button
+          onClick={handleInvite}
+          disabled={isDisabled}
+          className={invitebtn(isDisabled)}
+        >
+          초대
+        </button>
+      </div>
+    </Modal>
   );
 }
