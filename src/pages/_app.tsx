1badc318a6dcaf7afafb3380f6486ea6ac070d90
aNN-algorithm <kyukyu0314@naver.com>
2025-09-11 16:19:22 +0900

    Fix: 엑세스 토큰 로컬 스토리지에 저장

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,9 @@
 import type { AppProps } from "next/app";
 import "@/styles/globals.css";
 import localFont from "next/font/local";
+import { bootstrapAuth } from "@/stores/bootstrapAuth";
+
+bootstrapAuth();
 
 const pretendard = localFont({
   src: [
